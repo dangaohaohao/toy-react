70a7f230274748fb5afe33319b7f105329673e44
yuzhenliu <yuzhenliu163@163.com>
2020-09-05 18:17:26 +0800

    feat: 环境搭建

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # toy-react 实现简易版 react
+
+- 项目初始化 `npm init -y` `git init` 配置 `.gitignore` 文件
+- 安装 ` yarn add webpack webpack-cli -D` 配置 `webpack.config.js` 文件已经 `package.json` 的 `script` 命令
+- 安装 babel ` yarn add babel-loader @babel/core @babel/preset-env @babel/plugin-transform-react-jsx -D` 配置`webpack.config.js`文件和 `.babelrc` 文件
+- `.babelrc` 中的 `plugin` 可配置 `pragma`参数，配置修改`React.createElement`
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,26 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const {
+  CleanWebpackPlugin
+} = require("clean-webpack-plugin");
+
 module.exports = {
-    mode: 'development',
-    entry: {
-        main: './main.js'
-    },
-    optimization: {
-        minimize: false,
-    }
-}
+  mode: "development",
+  entry: {
+    main: "./main.js",
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      use: "babel-loader",
+    }, ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./main.html",
+    }),
+  ],
+  optimization: {
+    minimize: false,
+  },
+};
