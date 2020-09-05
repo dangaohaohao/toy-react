8c8dd576459773476e9bb2639890b0887251a3a9
yuzhenliu <yuzhenliu163@163.com>
2020-09-05 19:04:37 +0800

    feat: 基础的 jsx 语法

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
 for (let i of [1, 2, 3]) {
-    console.log(i);
+  console.log(i);
 }
 
-<div />
+function createElement(tagName, attributes, ...children) {
+  let ele = document.createElement(tagName);
+  for (const key in attributes) {
+    if (attributes.hasOwnProperty(key)) {
+      ele.setAttribute(key, attributes[key]);
+    }
+  }
+  for (let child of children) {
+    if (typeof child === 'string') {
+      child = document.createTextNode(child);
+    }
+    ele.append(child);
+  }
+  return ele;
+}
+
+document.body.append(
+  <div id="#root" class="root" key="root">
+    <div>yuzhenliu</div>
+    <span></span>
+  </div>
+);
